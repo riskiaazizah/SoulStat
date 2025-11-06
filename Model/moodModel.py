d3ddeb02914ef0b68d39f2471901c6baa030eab4
Riskia Nur Azizah Ismail <riskiaazizah7@gmail.com>
2025-11-06 20:13:02 +0800

    inisiasi

--- Model/moodModel.py
+++ Model/moodModel.py
@@ -2,4 +2,13 @@ import datetime
 import random
 
 class moodModel:
-    pass
+    def _init_(self):
+        self.data_mood = []
+
+    def add_mood(self, mood):
+        tanggal = datetime.date.today().strftime("%Y-%m-%d")
+        self.data_mood.append({"tanggal": tanggal, "mood": mood})
+        return tanggal
+
+    def get_all_moods(self):
+        return self.data_mood
