7191bc64b1b20f7f28842042e3dfaee2a0c23d34
mtaqwin <muhtakwiny@gmail.com>
2025-11-06 20:19:08 +0800

    def delete mood

--- Controller/moodController.py
+++ Controller/moodController.py
@@ -2,4 +2,14 @@ from Model.moodModel import MoodModel
 from View.cli_view import CLIView
 
 class moodController:
-    pass
+    def delete_mood(self):
+        data = self.model.get_all_moods()
+        self.view.display_mood_table(data)
+        if not data:
+            return
+        try:
+            index = int(input("Masukkan nomor yang ingin dihapus: ")) - 1
+            deleted = self.model.delete_mood(index)
+            self.view.confirm_delete(deleted)
+        except ValueError:
+            self.view.show_error("Input harus berupa angka.")
--- Model/moodModel.py
+++ Model/moodModel.py
@@ -2,4 +2,7 @@ import datetime
 import random
 
 class moodModel:
-    pass
+    def delete_mood(self, index):
+        if 0 <= index < len(self.data_mood):
+            return self.data_mood.pop(index)
+        return None
