07f1cd9fda47ee419ba70ffe6030a75a23acf761
Riskia Nur Azizah Ismail <riskiaazizah7@gmail.com>
2025-11-08 16:41:52 +0800

    add : main

--- main.py
+++ main.py
@@ -3,4 +3,21 @@ from View.view import View
 from Controller.moodController import MoodController
 
 def main():
-    pass
+    model = MoodModel()
+    view = View()
+    controller = MoodController(model, view)
+    
+    while True :
+        pilihan = view.show_menu()
+        
+        match pilihan :
+            case "1" :
+                controller.tambahMood()
+            
+            case "2" :
+                controller.tampilkanRekomendasi()
+            
+        
+                
+        
+        
